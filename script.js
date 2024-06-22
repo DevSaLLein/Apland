83fd7ab470ea515f5d2864a2e3422e10fbc2d2e3
DevSaLLein <isaac.andrade3@aluno.ce.gov.br>
2024-06-22 11:13:56 -0300

    add menu mobile

--- script.js
+++ script.js
@@ -9,10 +9,18 @@ window.addEventListener('scroll', () => {
 window.addEventListener('load', () => {
     
     let toggleButton = document.querySelector('#toggleButton');
+
+    let menuMobile = document.querySelector('.menu-mobile');
+    let hamburger = document.querySelector('.input-menu');
     
     toggleButton.addEventListener('click', () => {
 
         toggleButton.children.item(0).classList.toggle('toggleModeSun');
         document.body.classList.toggle('darkMode');
     });
+
+    hamburger.addEventListener('click', () => {
+
+        menuMobile.classList.toggle('menu-mobile-active');
+    })
 });
